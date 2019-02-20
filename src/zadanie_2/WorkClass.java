8df360fbefbca883e036ec23129058581bf6e7a8
LeszekSte <34620437+LeszekSte@users.noreply.github.com>
2019-02-20 13:33:32 +0100

    Update WorkClass.java

--- src/zadanie_2/WorkClass.java
+++ src/zadanie_2/WorkClass.java
@@ -1,32 +1,45 @@
 package zadanie_2;
 
+
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-
-import java.util.List;
-import java.util.Scanner;
-import java.util.TreeSet;
+import java.util.*;
 
 public class WorkClass {
 
+    List<Integer> numbers = new ArrayList<>();
 
     public List<Integer> readDate(String file) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(file));
 
-            Scanner scanner = new Scanner(new File(file));
-            List<Integer> numbers = new ArrayList<>();
-            while (scanner.hasNextInt()){
-                numbers.add(scanner.nextInt());
-            }
-            System.out.println("koniec");
-            return numbers;
+        while (scanner.hasNextInt()) {
+            numbers.add(scanner.nextInt());
         }
-
-
-
+        System.out.println("koniec");
+        return numbers;
+    }
 
     public void printList(TreeSet<Integer> occurrences) {
-        System.out.println("Liczbu unikalne" + occurrences.size());
+        Iterator<Integer> iterator = occurrences.iterator();
+        List<Integer> listaDoDruku = new ArrayList<>();
+        System.out.println("Ilość unikalnych liczb " + occurrences.size());
 
+        while (iterator.hasNext()) {
+            Integer next = iterator.next();
+            listaDoDruku.add(next);
+        }
+        for (int i = 0; i < listaDoDruku.size(); i++) {
+            System.out.println(listaDoDruku.get(i) + " - liczba wystąpień " + countnNumers(numbers, listaDoDruku.get(i)));
+        }
+    }
+
+    private int countnNumers(List<Integer> list, Integer next) {
+        int sum = 0;
+        for (Integer integer : list) {
+            if (next.equals(integer)) {
+                sum++;
+            }
+        }
+        return sum;
     }
 }
